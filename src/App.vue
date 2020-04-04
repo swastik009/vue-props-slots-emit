3ded768fd0e992ba097cdb2a41c9f1711bd6f7fd
swastik007 <swastik.thapaliya@gmail.com>
2020-04-04 22:39:10 +0545

    first commit

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="app">
+      <header>
+        <span> {{header}}</span>
+      </header>
+
+<!--- Custom event listener jun component bata generate bhiaracha tesle nai listen garcha -->
+    <HelloWorld @customEventFromChild="methodOfParent($event)" :msg="dataProps" :funcThroughProps="data => `${data.id}) ${data.name}`">
+      
+
+      <h3>
+          {{defaultSlot}}
+      </h3>
+      <h3 slot="mySlot">
+          {{namedSlot}}
+      </h3>
+
+      <!-- scoped slot uses parameter passed from the child defined in slot-scope attribute
+      arko way to incorporate parent data into child using html tags -->
+      <template slot-scope="props" slot="scopedSlot">
+         {{props.data.id}}) {{props.data.name}} <span style="color: red">{{dataFromParent}}</span>
+      </template>
+
+    </HelloWorld>
+
+
+    <footer>
+      <div></div>
+       <span >{{footer}} </span>
+    </footer>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      defaultSlot: "1)Data passed from default slot",
+      namedSlot : "2)Data passed from named slot",
+      dataProps : "Message from using prop",
+      dataFromParent: "Message from parent",
+      header: "Vue Header",
+      footer: "Vue footer"
+    }
+  },
+  methods:{
+      methodOfParent(data){
+        this.header = data;
+        this.footer = data;
+      }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+
+<style type="text/css" scoped>
+ header{
+    background-color: grey; 
+    height: 50px; 
+    margin-bottom: 20px; 
+    text-align: center;
+
+  }
+
+  footer{
+    background-color: grey; 
+    height: 50px; 
+    margin-bottom: 20px; 
+    text-align: center;
+
+  }
+</style>
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    Slot
+    <hr>
+    <slot></slot>
+    <slot name="mySlot"></slot>
+
+    Scoped Slots
+    <hr>
+    <div v-for="(data) in myDummyData" :key="data.id">
+        <slot name="scopedSlot" :data="data">
+        </slot>
+    </div>
+
+    <br>
+    Props
+    <hr>
+    <h3>{{msg}}</h3>
+
+    props from function
+    <hr>
+    <div v-for="(data) in myDummyData2" :key="data.id">
+        {{funcThroughProps(data)}}
+    </div>
+
+    Emit
+    <hr>
+    <button @click="customEvent">
+      Button of child
+    </button>
+
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    funcThroughProps: Function
+  },
+  data(){
+    return{
+        myDummyData: [
+          {
+            id : '1',
+            name : 'John doe'
+          },
+          {
+            id : '2',
+            name : 'Jane doe'
+          }
+
+        ],
+
+         myDummyData2: [
+          {
+            id : '3',
+            name : 'Mary doe'
+          },
+          {
+            id : '4',
+            name : 'Harry doe'
+          }
+
+        ],
+
+        dataFromChild: 'Data from Child'
+
+    }
+  },
+
+  methods:{
+    //this.$emit le chai custom event generate gariracha jasle garda child component ko data parent component le use garna paaos
+    customEvent(){
+      this.$emit('customEventFromChild',this.dataFromChild);
+    }
+  }
+}
+</script>
+
